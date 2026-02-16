a7cc7fe6572b52ca11cfe06b2b98ad28b0dcf948
NV-7 <naviboun@gmail.com>
2026-02-15 18:14:08 -0600

    Camera and 3d movement
    Created a free body camera that looks at player and updated 3d movement so that it updates using physics

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -2,58 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 public class PlayerBehavior : MonoBehaviour
 
 {
     public float speed = 10;
+    public Vector3 input_direction;
+    public Rigidbody rb;
+    public float rotationSpeed = 720f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float x_movement = 0;
+        float z_movement = 0;
 
-        Vector3 pos = transform.position;
+        if (Keyboard.current.wKey.isPressed)
+        {
+            z_movement += 1;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            x_movement += -1;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            z_movement += -1;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            x_movement += 1;
+        }
 
-        if (Keyboard.current.upArrowKey.isPressed == true)
-        {
-            
-                Vector3 newPos = transform.position;
-                newPos.y = newPos.y + speed * Time.deltaTime;
-                transform.position = newPos;
-            
-        }
-        if (Keyboard.current.leftArrowKey.isPressed == true)
-        {
-            
-                Vector3 newPos = transform.position;
-                newPos.x = newPos.x - speed * Time.deltaTime;
-                transform.position = newPos;
-              
-            
-        }
-        if (Keyboard.current.downArrowKey.isPressed == true)
-        {
-            Vector3 newPos = transform.position;
-            newPos.y = newPos.y - speed * Time.deltaTime;
-            transform.position = newPos;
-            
+        input_direction = new Vector3(x_movement, 0, z_movement).normalized;
 
-        }
-        if (Keyboard.current.rightArrowKey.isPressed == true)
+      
+    }
+    private void FixedUpdate()
+    {
+        if (input_direction != Vector3.zero)
         {
-            Vector3 newPos = transform.position;
-            newPos.x = newPos.x + speed * Time.deltaTime;
-            transform.position = newPos;
-           
+            rb.velocity = new Vector3(input_direction.x * speed, rb.velocity.y, input_direction.z * speed);
+            Quaternion targetRotation = Quaternion.LookRotation(input_direction);
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+        }
+        else
+        {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
         }
     }
-    
-    
+
+
 }
