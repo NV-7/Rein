f5f6b7a843b33fa0b1154f785a1b201ccb29a8c2
NV-7 <naviboun@gmail.com>
2026-04-15 18:42:41 -0500

    Updated Model and Player
    Front of model now faces Z direction. Player uses unity's new input manager. Player always faces the mouse.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Vector3 movementInput;
+    private Vector2 aimInput;
+    private Vector2 movementInput;
     private Rigidbody playerRb;
+    private Camera mainCam;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        mainCam = Camera.main;
     }
 
     // Update is called once per frame
@@ -24,15 +28,43 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         PlayerMovement();
+        lookAtMouse();
     }
 
     void PlayerMovement()
     {
-        playerRb.MovePosition(playerRb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+        Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+        playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
-    public void OnMove(InputValue value)
+    void lookAtMouse()
+    {
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+
+        Plane plane = new Plane(Vector3.up, transform.position);
+
+        if(plane.Raycast(ray, out float distance))
+        {
+            Vector3 intersectPoint = ray.GetPoint(distance);
+
+            Vector3 playerToMouseDirection = intersectPoint - transform.position;
+            playerToMouseDirection.y = 0;
+
+            Quaternion rotation = Quaternion.LookRotation(playerToMouseDirection);
+            transform.rotation = rotation;
+
+        }
+
+        
+    }
+
+    void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
+    }
+
+    void OnAim(InputValue value)
+    {
+        aimInput = value.Get<Vector2>();
     }
 }
